63267ff9aa889f38f8a1d43a99490b2f03e76d64
ved1beta <vedantthote2019@gmail.com>
2024-11-27 17:40:41 +0530

    data processing done

--- simple-rag-model/src/data_processing.py
+++ simple-rag-model/src/data_processing.py
@@ -4,6 +4,7 @@ import fitz
 from tqdm.auto import tqdm
 import spacy
 import random
+import pandas as pd
 
 def text_formatter(text: str) -> str:
     """
@@ -31,7 +32,7 @@ def open_read_pdf(pdf_path):
 nlp = spacy.blank("en")
 nlp.add_pipe("sentencizer")
 
-pages_text = open_read_pdf("data/raw/V Kishore Ayyadevara, Yeshwanth Reddy - Modern Computer Vision with PyTorch_ A Practical Roadmap Fro.pdf")
+pages_text = open_read_pdf("data.pdf")
 
 for item in tqdm(pages_text):
     doc = nlp(item["text"])
@@ -40,4 +41,45 @@ for item in tqdm(pages_text):
 
 
 random_page = random.sample(pages_text, k=1)
-random_page
+
+num_sentence_chunk_size = 10 
+
+# Create a function that recursively splits a list into desired sizes
+def split_list(input_list: list, 
+               slice_size: int) -> list[list[str]]:
+    """
+    Splits the input_list into sublists of size slice_size (or as close as possible).
+
+    For example, a list of 17 sentences would be split into two lists of [[10], [7]]
+    """
+    return [input_list[i:i + slice_size] for i in range(0, len(input_list), slice_size)]
+
+# Loop through pages and texts and split sentences into chunks
+for item in tqdm(pages_text):
+    item["sentence_chunks"] = split_list(input_list=item["sentences"],
+                                         slice_size=num_sentence_chunk_size)
+    item["num_chunks"] = len(item["sentence_chunks"])
+
+
+# Split each chunk into its own item
+pages_and_chunks = []
+for item in tqdm(pages_text):
+    for sentence_chunk in item["sentence_chunks"]:
+        chunk_dict = {}
+        
+        
+        # Convert Span objects to strings before joining
+        joined_sentence_chunk = "".join(str(sentence) for sentence in sentence_chunk).replace("  ", " ").strip()
+        joined_sentence_chunk = re.sub(r'\.([A-Z])', r'. \1', joined_sentence_chunk) # ".A" -> ". A" for any full-stop/capital letter combo 
+        chunk_dict["sentence_chunk"] = joined_sentence_chunk
+        # Get stats about the chunk
+        chunk_dict["chunk_char_count"] = len(joined_sentence_chunk)
+        chunk_dict["chunk_word_count"] = len([word for word in joined_sentence_chunk.split(" ")])
+        chunk_dict["chunk_token_count"] = len(joined_sentence_chunk) / 4 # 1 token = ~4 characters
+        
+        pages_and_chunks.append(chunk_dict)
+
+# How many chunks do we have?
+print(len(pages_and_chunks))
+
+df = pd.DataFrame(pages_and_chunks) 
